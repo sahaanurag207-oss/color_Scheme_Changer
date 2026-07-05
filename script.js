44184ff6912f437608cd09c042fbd72598f97ad0
Anurag Saha <sahaanurag207@gmail.com>
2026-07-06 00:19:59 +0530

    Restore button event listeners and background functions

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-// const buttons = document.querySelectorAll('.button');
-// const body = document.querySelector('body');
-// // document.body.addEventListener('click',(event)=>{
-// //     if(event.target == = document.body){
-// //         document.body.style.backgroundColor =  "rgb(112, 110, 110)";
-// //     }
-// // });
-// buttons.forEach(function (button) {
-//   console.log(button);
-//   button.addEventListener('click', function (e) {
-//     console.log(e);
-//     console.log(e.target);
-//     if (e.target.id === 'red') {
-//       body.style.backgroundColor = e.target.id;
-//     }
-//     if (e.target.id === 'white') {
-//       body.style.backgroundColor = e.target.id;
-//     }
-//     if (e.target.id === 'blue') {
-//       body.style.backgroundColor = e.target.id;
-//     }
-//     if (e.target.id === 'yellow') {
-//       body.style.backgroundColor = e.target.id;
-//     }
-//   });
-// });
 const body = document.body;
 
 // Function to change background to a gradient
